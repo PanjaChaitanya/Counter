d91a3ef5a4f67a53f512182e5955b71713c56b79
PanjaChaitanya <panjachaitanya23@gmail.com>
2024-12-13 21:19:14 +0530

    updated

--- src/Mycount.jsx
+++ src/Mycount.jsx
@@ -1,65 +1,91 @@
 import React from 'react';
-import "./Mycount.css";
-class Mycount extends React.Component{
-    constructor(){
-        super()
-        this.state={
-            count:10,
-            msg:'🟢 Starting Point 🟢'
-        }
+import './Mycount.css';
+
+class Mycount extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: '',
+      min: '',
+      max: '',
+      msg: '',
+      start:'',
+    };
+  }
+  setFun = () => {
+    var min = parseInt(document.getElementById('i1').value);
+    var start = parseInt(document.getElementById('i2').value);
+    var max = parseInt(document.getElementById('i3').value);
+    if (min < start && start < max) {
+      this.setState({
+        min: '',
+        count: start,
+        max: '',
+        start: '',
+        msg: '🟢 Starting Point 🟢',
+      });
+    } else {
+      alert('Please enter values in ascending order: Min < Start < Max');
     }
-    IncreaseFun(){
-       if(this.state.count<20){
-        this.setState({
-            count: this.state.count +1,
-            msg:''
-        })
-       }else{
-        this.setState({
-            msg:'⚠️ Increase limit reached ⚠️'
-        })
-       }
+  };
+  IncreaseFun = () => {
+    if (this.state.count < this.state.max) {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        msg: '',
+      }));
+    } else {
+      this.setState({
+        msg: '⚠️ Increase limit reached ⚠️',
+      });
     }
-    DecreaseFun(){
-        if(this.state.count>0){
-            this.setState({
-                count: this.state.count -1,
-                msg:''
-            })
-        }
-        else{
-            this.setState({
-                msg:'⚠️ Decrease limit reached ⚠️'
-            })
-        }
+  };
+  DecreaseFun = () => {
+    if (this.state.count > this.state.min) {
+      this.setState((prevState) => ({
+        count: prevState.count - 1,
+        msg: '',
+      }));
+    } else {
+      this.setState({
+        msg: '⚠️ Decrease limit reached ⚠️',
+      });
     }
-    resetFun(){
-        this.setState({
-            count:10,
-            msg:'🟢 Starting Point 🟢'
-        })
-        
-    }
-    render(){
-        return(
-            <>
-            <header>INTERACTIVE COUNTER</header>
-            <div id='container'>
-                <div id='countContainer'>
-                    <h1>{this.state.count}</h1>
-                </div>
-                <div id="msgContainer">
-                    <p>{this.state.msg}</p>
-                </div>
-                <div id='buttonsContainer'>
-                    <button id='increase' onClick={()=>this.IncreaseFun()}>🢁</button>
-                    <button id='reset' onClick={()=>this.resetFun()}>⟳</button>
-                    <button id='decrease' onClick={()=>this.DecreaseFun()}>🢃</button>
-                </div>
-            </div>
-            </>
-        );
-    }
+  };
+  resetFun = () => {
+    this.setState({
+      count: this.state.start,
+      msg: '',
+    });
+  };
+  render() {
+    return (
+      <>
+        <header>INTERACTIVE COUNTER</header>
+        <div id="container">
+          <div id="inputContainer">
+            <input type="number" id="i1" placeholder="Enter Minimum Value" />
+            <input type="number" id="i2" placeholder="Enter Start Value" />
+            <input type="number" id="i3" placeholder="Enter Maximum Value" />
+          </div>
+          <div id='setBtn'>
+            <button onClick={this.setFun}>Set</button>
+          </div>
+          <div id="countContainer">
+            <h1>{this.state.count}</h1>
+          </div>
+          <div id="msgContainer">
+            <p>{this.state.msg}</p>
+          </div>
+          <div id="buttonsContainer">
+            <button id="increase" onClick={this.IncreaseFun}>🢁</button>
+            <button id="reset" onClick={this.resetFun}>⟳</button>
+            <button id="decrease" onClick={this.DecreaseFun}>🢃</button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Mycount;
