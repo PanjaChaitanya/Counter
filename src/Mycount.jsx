b810000ba77c6f52aa729eb15d03d2d6029dbc34
shivu1919 <shivamthakur.ss129@gmail.com>
2024-12-17 18:08:56 +0530

    commit in main branch

--- src/Mycount.jsx
+++ src/Mycount.jsx
@@ -61,7 +61,7 @@ class Mycount extends React.Component {
   render() {
     return (
       <>
-        <header>I-COUNTER</header>
+        <header>In-COUNTER</header>
         <div id="container">
           <div id="inputContainer">
             <input type="number" id="i1" placeholder="Enter Minimum Value" />
